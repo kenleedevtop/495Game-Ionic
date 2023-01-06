843e4478fcee0da03e2fee2904db2d9e75bcfc42
kenleedevtop <kenleedevtop@gmail.com>
2023-01-06 20:35:05 +0100

    Fix issues

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,5 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonPage, IonRow } from '@ionic/react';
-import { useState } from 'react';
+import { IonButton, IonCol, IonContent, IonGrid, IonPage, IonRow, IonToast, useIonToast } from '@ionic/react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import TextInput from '../components/TextInput';
 import './Game.scss';
@@ -13,38 +13,142 @@ interface ContainerProps {
   setRoom: any;
 }
 
-const Game: React.FC<ContainerProps> = ({socket}) => {
+const Game: React.FC<ContainerProps> = ({ socket, id, room, setRoom }) => {
   const [roll, setRoll] = useState<any>(0);
   const [factor, setFactor] = useState<any>(0);
-  // eslint-disable-next-line
-  const [activename, setActiveName] = useState<string>("TOM");
+  const [selectedPlayer, setSelectPlayer] = useState<string>("");
   const [value, setValue] = useState<number>(490);
   const [realValue, setRealValue] = useState<string>("49O")
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [players, setPlayers] = useState([]);
+  const [playerDebts, setPlayerDebts] = useState([]);
+  const [currentTurn, setCurrentTurn] = useState("");
+  const [myTurn, setMyTurn] = useState(false);
+
+  const [presentToast] = useIonToast();
+
   const history = useHistory();
+
+  useEffect(() => {
+    // @ts-ignore
+    socket.current.on("game_players", (players) => {
+      setPlayers(players);
+    });
+    // @ts-ignore
+    socket.current.on("player_debts", (debts) => {
+      const myDebt = debts.filter((item: any) => item.id === id);
+      setValue(myDebt[0].debt);
+      let strValue = myDebt[0].debt.toString();
+      strValue = strValue.replace(/0/g, 'O');
+      strValue = strValue.replace(/1/g, 'l');
+      setRealValue(strValue);
+      setPlayerDebts(debts);
+    });
+
+    // @ts-ignore
+    socket.current.on("your_turn", (newplayers, player_id) => {
+      setPlayers(newplayers);
+      const turnedPlayer: any = newplayers.filter((player: any) => player.id === player_id)
+      if (player_id === id) {
+        setCurrentTurn("You");
+      } else {
+        setCurrentTurn(turnedPlayer[0].nickname);
+      }
+      if (player_id === id) {
+        setMyTurn(true);
+      } else {
+        setMyTurn(false);
+      }
+    });
+
+    // @ts-ignore
+    socket.current.on("forgive", (newid, amount, players) => {
+      let Prefix = "";
+      if (newid === id) {
+        Prefix = "You"
+      } else {
+        const player = players.filter((item: any) => item.id === newid);
+        Prefix = player[0].nickname
+      }
+      presentToast({
+        message: `${Prefix} forgived ${amount} of debts`,
+        duration: 3000
+      })
+    });
+
+    // @ts-ignore
+    socket.current.on("update", (message) => {
+      presentToast({
+        message: message,
+        duration: 3000
+      })
+    });
+
+    // @ts-ignore
+    socket.current.on("transgress", (from, to, amount, players) => {
+      let Prefix = "";
+      let Subfix = "";
+      if (to === id) {
+        Prefix = "You"
+      } else {
+        const player = players.filter((item: any) => item.id === to);
+        Prefix = player[0].nickname
+      }
+      if (from === id) {
+        Subfix = "You"
+      } else {
+        const player = players.filter((item: any) => item.id === to);
+        Subfix = player[0].nickname
+      }
+      presentToast({
+        message: `${Subfix} transgressed ${amount} of debts to ${Prefix}`,
+        duration: 3000
+      })
+    });
+
+    // @ts-ignore
+    socket.current.on("end_game", (message) => {
+      presentToast({
+        message: message,
+        duration: 3000
+      })
+      // history.push('/lobby')
+      setRoom("");
+    });
+    //eslint-disable-next-line
+  }, [socket.current])
 
   const handleForgive = () => {
     const sum = parseInt(roll) * parseInt(factor);
     let subValue = value - sum;
-    if (subValue <= 0)  {
+    if (subValue <= 0) {
       subValue = 0;
     }
-    setValue(subValue);
-   let strValue = subValue.toString();
-   strValue = strValue.replace(/0/g, 'O');
-   strValue = strValue.replace(/1/g, 'l');
-
-   setRealValue(strValue);
-
-   if(subValue === 0) {
-    history.push('/over')
-   }
+    socket.current.emit("forgive", { id, room, amount: sum, debt: subValue });
+    socket.current.emit("turn_over", { room });
+    if (subValue === 0) {
+      socket.current.emit('success_game', { name: id, room });
+      history.push('/over')
+    }
   }
 
   const handleTransgress = () => {
-
+    if (selectedPlayer) {
+      const sum = parseInt(roll) * parseInt(factor);
+      const othervalue: any = playerDebts.filter((item: any) => item.id === selectedPlayer)
+      let subValue = othervalue[0].debt + sum;
+      socket.current.emit("transgress", { from: id, to: selectedPlayer, room, amount: sum, debt: subValue });
+      socket.current.emit("turn_over", { room });
+    } else {
+      setShowAlert(true)
+    }
   }
 
   const handleExit = () => {
+    socket.current.emit('leave_game', { name: id, room });
+    socket.current.emit('join_lobby');
+    setRoom("");
     history.push('/lobby')
   }
 
@@ -58,29 +162,44 @@ const Game: React.FC<ContainerProps> = ({socket}) => {
     setFactor(value);
   }
 
-  const players = ["JOHN", "TOM", "JERRY", "CHRIS", "GINA", "STAR", "WOLF", "DOG"]
+  const handleSelectPlayer = (id: any) => {
+    setSelectPlayer(id);
+  }
 
   return (
     <IonPage className='gamePage'>
       <IonContent >
-          <div className='yourname'>
-            TOM
-          </div>
+        <div className='yourname'>
+          {currentTurn}
+        </div>
         <IonRow className='content'>
           <IonGrid className='item-container'>
             {
-              players.map((name: any, index: any) => {
+              players.map((item: any, index: any) => {
                 let classname = "room-item"
-                if (name === activename) {
+                if (item.id === selectedPlayer) {
                   classname = "active-room-item"
                 } else {
                   classname = "room-item"
                 }
+                const myDebt: any = playerDebts.filter((debt: any) => debt.id === item.id);
+
+                let pname = ""
+                if (item.id === id) {
+                  pname = "You"
+                } else {
+                  pname = item.nickname
+                }
                 return (
-                  <IonRow className={classname}>
-                    <IonCol>{name}</IonCol>
+                  <IonRow key={index} className={classname}
+                    onClick={() => handleSelectPlayer(item.id)}
+                  >
+                    <IonCol>{pname}</IonCol>
                     <IonCol size="auto">
-                      <div style={{ width: "50px" }}>{realValue}</div>
+                      {
+                        myDebt.length > 0 &&
+                        <div style={{ width: "50px" }}>{myDebt[0].debt}</div>
+                      }
                     </IonCol>
                   </IonRow>
                 )
@@ -103,8 +222,8 @@ const Game: React.FC<ContainerProps> = ({socket}) => {
                   onChange={handleRoll}
                 />
               </div>
-              <p style={{color: "white", marginTop: '46px'}}>x</p>
-              <div  style={{ width: '100px' }}>
+              <p style={{ color: "white", marginTop: '46px' }}>x</p>
+              <div style={{ width: '100px' }}>
                 <h1>FACTOR</h1>
                 <TextInput
                   elemenName='factor'
@@ -117,12 +236,20 @@ const Game: React.FC<ContainerProps> = ({socket}) => {
               </div>
             </div>
             <div className='button-container'>
-              <IonButton className='color2' onClick={handleForgive}><p>FORGIVE(-)</p></IonButton>
-              <IonButton className='color3' onClick={handleTransgress}><p>TRANSGRESS(+)</p></IonButton>
+              <IonButton className='color2' disabled={!myTurn} onClick={handleForgive}><p>FORGIVE(-)</p></IonButton>
+              <IonButton className='color3' disabled={!myTurn} onClick={handleTransgress}><p>TRANSGRESS(+)</p></IonButton>
               <IonButton className='color1' onClick={handleExit}><p>EXIT</p></IonButton>
             </div>
           </div>
         </IonRow>
+        <IonToast
+          cssClass={'custom-class'}
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          duration={3000}
+          message="You should select the other Player for transgress"
+          buttons={['OK']}
+        />
       </IonContent>
     </IonPage>
   );
--- src/pages/Ready.tsx
+++ src/pages/Ready.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonPage, IonRow } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonPage, IonRow, IonToast } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import TextInput from '../components/TextInput';
@@ -13,45 +13,83 @@ interface ContainerProps {
   setRoom: any;
 }
 
-const Ready: React.FC<ContainerProps> = ({ socket, id , room}) => {
+const Ready: React.FC<ContainerProps> = ({ socket, id, room, setRoom }) => {
   const history = useHistory();
   const [nickname, setNickName] = useState<string>("");
   // eslint-disable-next-line
-  const [start, setStart] = useState(false);
+  const [started, setStarted] = useState(false);
   const [ready, setReady] = useState(false);
   const [playerCount, setPlayerCount] = useState(0);
   const [players, setPlayers] = useState<any[]>([]);
+  const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
     // @ts-ignore
     socket.current.on("player_names", (names) => {
       setPlayers(names);
     });
+
+    // @ts-ignore
+    socket.current.on("room_state", (started) => {
+      setStarted(started);
+    });
+
     // @ts-ignore
     socket.current.on("max_member", (count) => {
       setPlayerCount(count);
     });
-    socket.current.on("start_game", () => {
-      setStart(true);
+
+     // @ts-ignore
+    socket.current.on("start_game", (players) => {
+      handleJoin(players);
     });
     // @ts-ignore
     // eslint-disable-next-line 
   }, [socket.current]);
 
+
+
   const handleStart = () => {
-    history.push('/game')
+    const readyplayer = players.filter((player: any) => player.nickname.length > 0)
+    if (readyplayer.length >= 2) {
+      socket.current.emit('start_game', { room });
+    } else {
+      setShowAlert(true);
+    }
   }
 
+  const handleJoin = (newplayers: any) => {
+    const me = newplayers.filter((player: any) => player.id === id);
+    if (me.length > 0) {
+      socket.current.emit('join_game', { room , id});
+      setReady(false);
+      history.push('/game')
+    }
+  }
+
+  const handleStart1 = () => {
+    const readyplayer = players.filter((player: any) => player.nickname.length > 0)
+    if (readyplayer.length >= 2) {
+      socket.current.emit('join_game', { room });
+      setReady(false);
+      history.push('/game')
+    } else {
+      setShowAlert(true);
+    }
+  }
+
+
   const handleReady = () => {
-    socket.current.emit('ready', {room, id, nickname});
+    socket.current.emit('ready', { room, id, nickname });
     setReady(true);
   }
 
 
   const handleBack = () => {
-    socket.current.emit('leave_room', {name: id, room});
+    socket.current.emit('leave_room', { name: id, room });
     socket.current.emit('join_lobby');
     setReady(false);
+    setRoom("");
     history.push("/lobby")
   }
 
@@ -115,16 +153,31 @@ const Ready: React.FC<ContainerProps> = ({ socket, id , room}) => {
               </div>
               <div className='button-container'>
                 {
-                  !ready ? 
-                  <IonButton disabled={nickname === ""} className='color2' onClick={handleReady}><p>READY</p></IonButton>
-                  :
-                  <IonButton  className='color2' onClick={handleStart}><p>START</p></IonButton>
+                  !ready ?
+                    <IonButton disabled={nickname === ""} className='color2' onClick={handleReady}><p>READY</p></IonButton>
+                    :
+                    <>
+                      {
+                        started ?
+                          <IonButton className='color2' onClick={handleStart1}><p>JOIN</p></IonButton>
+                          :
+                          <IonButton className='color2' onClick={handleStart}><p>START</p></IonButton>
+                      }
+                    </>
                 }
                 <IonButton className='color3' onClick={handleBack}><p>LOBBY</p></IonButton>
               </div>
             </div>
           </div>
         </IonRow>
+        <IonToast
+          cssClass={'custom-class'}
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          message="A minimum of two ready players are required to start the game."
+          duration={3000}
+          buttons={['OK']}
+        />
       </IonContent>
     </IonPage>
   );
